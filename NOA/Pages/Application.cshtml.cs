85dc0dca4d691e4d810dc56a60e7b953df8448d6
Thomas Buchanan <tbuchanan735@gmail.com>
2020-01-09 23:06:39 +0000

    Created Application View and linked to Shared Layout

---
+++ NOA/Pages/Application.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NOA.Pages
+{
+    public class ApplicationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
